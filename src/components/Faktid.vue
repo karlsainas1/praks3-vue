7725c5b3fc61afa15fb1a522d3a84c5dfceca203
karlsainas1 <karl.sainas@khk.ee>
2018-10-11 14:02:19 +0300

    tehtud asi
    Signed-off-by: karlsainas1 <karl.sainas@khk.ee>

---
+++ src/components/Faktid.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+
+ 
+   <h1>Numbri faktid</h1>
+  <input type="number" placeholder="sisesta number" min="0" v-model="number" >
+      
+          <p> {{ data }} </p>
+
+
+</div>
+</template>
+
+<script>
+    const axios = require('axios')
+export default {
+  name: 'Faktid',
+  data(){
+      return{
+        data:'',
+        number: 0 
+        
+      }
+  
+  },
+    watch:{
+        number() {
+         if(this.number !== '')  {   
+            
+            let url = `http://numbersapi.com/${this.number}`        
+        
+            axios.get(url)
+                .then(response => {
+                  this.data=response.data
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+        
+        }
+        }
+        },
+    
+    methods:{
+       
+        },
+   
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    div{    margin:50px;
+    }
+</style>
