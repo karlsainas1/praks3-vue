2bc0aa4740b93f6a9d074ce39a0e88ac71727a28
karlsainas1 <karl.sainas@khk.ee>
2018-10-11 15:37:39 +0300

    töötab aga kõik on punast teksti täis 10/10 Signed-off-by: karlsainas1 <karl.sainas@khk.ee>

--- src/components/Faktid.vue
+++ src/components/Faktid.vue
@@ -4,10 +4,10 @@
  
    <h1>Numbri faktid</h1>
   <input type="number" placeholder="sisesta number" min="0" v-model="number" >
-      
-          <p> {{ data }} </p>
-
-
+       <input type="number" placeholder="sisesta inc" min="0" v-model="inc" >
+           
+      <p v-for="value in data" :key="index"> {{ value }} </p>
+                
 </div>
 </template>
 
@@ -17,39 +17,55 @@ export default {
   name: 'Faktid',
   data(){
       return{
-        data:'',
-        number: 0 
-        
+      
+          data:'',
+        number: 0, 
+        inc:0
       }
   
   },
     watch:{
-        number() {
-         if(this.number !== '')  {   
-            
-            let url = `http://numbersapi.com/${this.number}`        
-        
-            axios.get(url)
-                .then(response => {
-                  this.data=response.data
-            })
-            .catch(error =>{
-                console.log(error)
-            })
-        
-        }
-        }
+        number(){
+            this.getResponse()
         },
+        inc(){
+            this.getResponse()
+        } 
+    },
+
+        
     
     methods:{
        
-        },
+        getResponse () {
+            if(this.number !== '' && this.inc !== '')  {   
+                
+                let url = `http://numbersapi.com/${this.number}..${this.inc}`     
+                   
+        
+                axios.get(url)
+                    .then(response => {
+                      this.data=response.data
+                    items=response.data
+                })
+                .catch(error =>{
+                    console.log(error)
+                })
+        
+            }
+        
+           
+           
+           
+       }
+    },
    
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    div{    margin:50px;
-    }
+    
+    
+    
 </style>
